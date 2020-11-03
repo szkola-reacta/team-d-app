3ac3ff908aad459dd13b5dbfcdd63b519ae8c365
Artur Kempny <artur.kempny@gmail.com>
2020-11-03 18:53:45 +0100

    Add enzyme to project and simple test render App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import {shallow} from 'enzyme';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('Team D App');
   expect(linkElement).toBeInTheDocument();
 });
+
+it('Should render component App', () => {
+  shallow(<App />);
+})

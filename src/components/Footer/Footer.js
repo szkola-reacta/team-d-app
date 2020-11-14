301bf323fcec43bb8c1fc5ae93bae0f337622a14
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-11-14 16:03:13 +0100

    Create a footer layout (4)

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import './style.css';
+
 function Footer() {
     return (
         <footer>
-            Footer
+            <div className="firstRow">
+                <div><h3>About us</h3></div>
+                <div><h3>Menu</h3></div>
+                <div><h3>Social links</h3></div>
+            </div>
+            <div className="secondRow">Copyright © 2020 Szkoła Reacta's Team D. All Rights Reserved.</div>
         </footer>
     );
 }

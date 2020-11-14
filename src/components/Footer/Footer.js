b41f3bbf0760d229777f329b3fd57fec7013d286
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-11-14 16:19:22 +0100

    Style and fill sections of the Footer component (4)

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,23 @@ function Footer() {
     return (
         <footer>
             <div className="firstRow">
-                <div><h3>About us</h3></div>
-                <div><h3>Menu</h3></div>
-                <div><h3>Social links</h3></div>
+                <div className="column">
+                    <h3>About us</h3>
+                    <p>
+                        Satoshi Nakamoto launched lots of decentralisation when Litecoin required many decentralised application, for Augur limited some public key behind lots of multi signature. Blockchain thought some robust smart contract in a algorithm! Since OmiseGo bought few double spend, Augur could be many algo-traded vaporware, but Decred data mining few trusted hard fork!
+                    </p>
+                </div>
+                <div className="column">
+                    <h3>Menu</h3>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Places</li>
+                    </ul>
+                </div>
+                <div className="column">
+                    <h3>Social links</h3>
+                </div>
             </div>
             <div className="secondRow">Copyright © 2020 Szkoła Reacta's Team D. All Rights Reserved.</div>
         </footer>

09ab3b67d1e1a6e4f93dfdfbdc765ae1be3ddbd0
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-11-14 15:48:12 +0100

    Resolve wrong paths in App (4)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 import './App.css';
-import { Header, Footer } from './components';
 
 function App() {
   return (

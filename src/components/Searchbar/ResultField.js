fea624f87104dcf00e7b186af3543cfdcf95e265
Pawe <pawel.bareja@performancelabs.pl>
2020-11-17 10:27:00 +0100

    add toLowerCase method to filter
    needed to get rid of case sensitive filtering bug

--- src/components/Searchbar/ResultField.js
+++ src/components/Searchbar/ResultField.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 const LocationList = ({ attractionList, searchInput }) => {
+  searchInput = searchInput.toLowerCase();
   const locationList = attractionList
-    .filter((item) => item.title.includes(searchInput))
+    .filter((item) => item.title.toLowerCase().includes(searchInput))
     .map((filteredItem) => (
       <a key={`place${filteredItem.title}`} href={`url_jak_bedzie_w_dummyData`}>
         {filteredItem.title}

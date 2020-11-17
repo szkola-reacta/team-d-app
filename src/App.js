5e6580b17a4dbda1ef7764e21f92e4ceb8b2d5ee
Lukasz Modzelewski <lukaszmod@o2.pl>
2020-11-17 09:36:58 +0100

    Redirect to new Route 'page-not-found'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 
-import PlaceList from "./data/PlaceList";
+import PlaceList from './data/PlaceList';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import { PageNotFound } from './components/PageNotFound';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className='App'>
         <Header />
         <Switch>
-          <Route path="/" exact>
+          <Route path='/' exact>
             Home
             <PlaceList />
           </Route>
-          <Route path="/about">About</Route>
-          <Route path="/places">Places</Route>
+          <Route path='/about'>About</Route>
+          <Route path='/places'>Places</Route>
+          <Route
+            path='/page-not-found'
+            render={() => <PageNotFound path='/' />}
+          />
+          <Redirect to='page-not-found' />
         </Switch>
 
         <Footer />

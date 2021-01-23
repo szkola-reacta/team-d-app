1bae765e0dc3af0cf91401a6c3e2d0e8baed8a86
Lukasz Modzelewski <lukaszmod@o2.pl>
2021-01-23 22:31:25 +0100

    delete stop-scrolling feature, problems on desktop

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import NavList from './NavList';
 import NavIcon from './NavIcon';
 import Logo from './Logo';
@@ -7,19 +7,9 @@ import hamburger from './assets/menu-c.svg';
 
 function Nav() {
   const [isActive, setIsActive] = useState(false);
-
   const handleClick = () => {
     setIsActive(!isActive);
   };
-
-  useEffect(() => {
-    if (isActive) {
-      document.body.style.height = '100%';
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.cssText = null;
-    }
-  }, [isActive]);
 
   return (
     <nav className="Nav">

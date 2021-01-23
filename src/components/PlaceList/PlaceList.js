040651dd890a6dc6aa282d74d72813ba79f75ecd
Lukasz Modzelewski <lukaszmod@o2.pl>
2021-01-23 13:24:17 +0100

    remove uuid package, use new data.id

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { v4 as uuidv4 } from 'uuid';
 
 import { Place } from 'components/Place';
 import './style.scss';
@@ -9,7 +8,7 @@ const PlaceList = ({ places }) => {
     <div>
       <div className="PlaceList">
         {places.map((data) => (
-          <Place key={uuidv4()} data={data} />
+          <Place key={data.id} data={data} />
         ))}
       </div>
     </div>
